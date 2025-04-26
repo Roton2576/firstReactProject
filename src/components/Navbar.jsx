20403a4e5b5a3414fe8fce8d6efd34f4e2e69a79
programmingshikhbo <rdratan2576@gmail.com>
2025-04-26 12:25:48 +0600

    added hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
      const [isOpen, setIsOpen] = useState(false);
@@ -10,12 +11,12 @@ const Navbar = () => {
      }
 
      return (
-          <nav className="bg-green-800 text-white py-4 md:py-8">
+          <nav className="bg-green-800 text-white py-4 md:py-8 sticky top-0 z-10 border-b-2 border-green-500">
                <div className="container mx-auto flex justify-between items-center">
-                    <h3> React Practice </h3>
+                    <h3 className="text-xl font-bold"> React Practice </h3>
 
                     {/* Mobile Menu Button */}
-                    <div>
+                    <div className="md:hidden">
                          <button onClick={toggleMenu}>
                               {
                                    isOpen ? <IoMdClose /> : <FaBars />
@@ -23,15 +24,29 @@ const Navbar = () => {
                          </button>
                     </div>
 
-                    <ul className="flex space-x-4 md:space-x-6">
-                         <li> Home </li>
-                         <li> Products </li>
-                         <li> Blogs </li>
-                         <li> Contact </li>
-                         <li> About </li>
+                    <ul className="hidden md:flex space-x-4 md:space-x-6">
+                         <li> <Link to={'/'} className="hover:text-green-500"> Home </Link> </li>
+                         <li> <Link to={'products'} className="hover:text-green-500"> Products </Link> </li>
+                         <li> <Link to={'blogs'} className="hover:text-green-500"> Blogs </Link> </li>
+                         <li> <Link to={'contact'} className="hover:text-green-500"> Contact </Link> </li>
+                         <li> <Link to={'about'} className="hover:text-green-500"> About </Link> </li>
                     </ul>
 
-                    <button> Login </button>
+                    <button className="hidden md:block bg-white text-black px-4 py-1 rounded cursor-pointer hover:bg-slate-400"> Login </button>
+
+                    {/* Mobile menu collapsed */}
+                    <div className={`md:hidden w-full absolute bg-green-600 top-full left-0 ${isOpen ? 'block' : 'hidden'}`}>
+                         <ul className="flex flex-col items-center py-4 space-y-2">
+                              <li className="hover:text-green-500"> Home </li>
+                              <li className="hover:text-green-500"> Products </li>
+                              <li className="hover:text-green-500"> Blogs </li>
+                              <li className="hover:text-green-500"> Contact </li>
+                              <li className="hover:text-green-500"> About </li>
+                              <li className="hover:text-green-500">
+                                   <button className="bg-white text-black px-4 py-1 rounded cursor-pointer hover:bg-slate-400"> Login </button>
+                              </li>
+                         </ul>
+                    </div>
                </div>
           </nav>
      )
--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom"
+import Footer from "./Footer"
 import Navbar from "./Navbar"
 
 const Root = () => {
      return (
           <div>
                <Navbar />
+               <Outlet></Outlet>
+               <Footer />
           </div>
      )
 }
