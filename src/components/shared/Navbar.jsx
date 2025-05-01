b245565c0fe6920eadda4217c70a269732d0e6b9
programmingshikhbo <rdratan2576@gmail.com>
2025-05-01 19:16:43 +0600

    fix folder and file structure

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
      const location = useLocation();
 
      useEffect(() => {
-          setActiveLink(location.pathname || '/' );
+          setActiveLink(location.pathname || '/');
      }, [location.pathname]);
 
 
@@ -87,7 +87,9 @@ const Navbar = () => {
                          </li>
                     </ul>
 
-                    <button className="hidden md:block bg-white text-black px-4 py-1 rounded cursor-pointer hover:bg-slate-400"> Login </button>
+                    <Link to={'/login'}>
+                         <button className="hidden md:block bg-white text-black px-4 py-1 rounded cursor-pointer hover:bg-slate-400"> Login </button>
+                    </Link>
 
                     {/* Mobile menu collapsed */}
                     <div className={`md:hidden w-full absolute bg-green-600 top-full left-0 ${isOpen ? 'block' : 'hidden'}`}>
